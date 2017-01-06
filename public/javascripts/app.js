1d9e0c4d68ec52a76ca1b45dd9fb2eadea4531c1
Perry Craft <LjCraft12@gmail.com>
2017-01-05 23:02:06 -0500

    working on getting routes to work

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,25 +1,42 @@
 console.log('App.js is running');
 var app = angular.module('myApp', ['ui.router']);
 
+// Setting up routes to all pages. use double quotes to declare route name and routing
 app.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
         .when("home", {
-            url: "/",
-            templateUrl: "/templates/home.html",
-            controller: "homeController",
-            controllerAs: "$ctrl"
+            url: "/", //url for web address
+            templateUrl: "/templates/home.html", // destination of actual page to view
+            controller: "homeController", // controller for page that is viewing
+            controllerAs: "$ctrl" // alias for page controller
         })
-        .when("about",{
+    $stateProvider
+        .when("about", {
             url: "/about",
             templateUrl: "/templates/about.html",
             controller: "aboutController",
             controllerAs: "$ctrl"
         })
-        .when("profile",{
+    $stateProvider
+        .when("profile", {
             url: "/profile",
             templateUrl: "/templates/profile.html",
             controller: "profileController",
             controllerAs: "$ctrl"
         })
+    $urlRouterProvider.otherwise("/"); // If a page requested that doesn't exist go to the home page
 
 });
+//setting up controllers for all pages
+app.controller('homeController', function () { // controller for the homepage named homeController with a function method
+    this.title = 'Welcome home'; // this.title will pass the out put to the title of the current page
+    console.log('Home page is showing'); // console log to make sure everything is working the way it should
+});
+app.controller('aboutController', function () {
+    this.title = 'Welcome to the about page';
+    console.log('About page is showing')
+});
+app.controller('profileController', function () {
+    this.title = 'Welcome to the profile page';
+    console.log('Profile page is showing');
+})
